f229acae5b0f682a47adc5aeff072ec498694258
chrwit87 <christoph.wittmayer@gmx.at>
2017-05-19 14:05:58 +0200

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,30 @@
+## in this section, the file is read in and the date and
+## time columns are converted into date and time variables.
+
+bigdata <- read.table("household_power_consumption.txt",
+                      header=TRUE, sep=";")
+bigdata$Date <- as.Date(bigdata$Date, format="%d/%m/%Y")
+bigdata$Time <- strptime(bigdata$Time, format="%H:%M:%S")
+
+## here, a subset of the two days in february is created
+##and the time column is converted so that it contains
+##the full informations on date and time
+
+dfpower <- subset(bigdata,bigdata$Date=="2007-02-01" |
+                         bigdata$Date =="2007-02-02")
+dfpower[1:1440,"Time"] <- format(dfpower[1:1440,"Time"],
+                                 "2007-02-01 %H:%M:%S")
+dfpower[1441:2880,"Time"] <- format(dfpower[1441:2880,"Time"],
+                                 "2007-02-02 %H:%M:%S")
+
+## this is the plotting section
+
+hist(as.numeric(as.character(dfpower$Global_active_power)),
+     xlab="Global Active Power(kilowatts",
+     ylab="Frequency", main="Global Active Power",
+     col="red")
+     
+## here the created plot is written into a png-file.
+
+dev.copy(png, file="plot1.png", width=480, height=480)
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,50 @@
+## in this section, the file is read in and the date and
+## time columns are converted into date and time variables.
+
+bigdata <- read.table("household_power_consumption.txt",
+                      header=TRUE, sep=";")
+bigdata$Date <- as.Date(bigdata$Date, format="%d/%m/%Y")
+bigdata$Time <- strptime(bigdata$Time, format="%H:%M:%S")
+
+## here, a subset of the two days in february is created
+##and the time column is converted so that it contains
+##the full informations on date and time
+
+dfpower <- subset(bigdata,bigdata$Date=="2007-02-01" |
+                        bigdata$Date =="2007-02-02")
+dfpower[1:1440,"Time"] <- format(dfpower[1:1440,"Time"],
+                                 "2007-02-01 %H:%M:%S")
+dfpower[1441:2880,"Time"] <- format(dfpower[1441:2880,"Time"],
+                                    "2007-02-02 %H:%M:%S")
+
+## this is the plotting section
+
+par(mfrow = c(2,2))
+
+plot(dfpower$Time, as.numeric(as.character(
+      dfpower$Global_active_power)),type="l",
+     xlab="", ylab="Global Active Power")
+
+plot(dfpower$Time, as.numeric(as.character(
+      dfpower$Voltage)),type="l",
+     xlab="datetime", ylab="Voltage")
+
+plot(dfpower$Time, as.numeric(as.character(
+      dfpower$Sub_metering_1)),type="l",
+     xlab="", ylab="Energy sub metering")
+lines(dfpower$Time,as.numeric(as.character
+                              (dfpower$Sub_metering_2)),col="red")
+lines(dfpower$Time,as.numeric(as.character
+                              (dfpower$Sub_metering_3)),col="blue")
+legend("topright", lty=1,col=c("black", "red", "blue"),
+       legend=c("Sub_metering_1","Sub_metering_2",
+                "Sub_metering_3"))
+
+plot(dfpower$Time, as.numeric(as.character(
+      dfpower$Global_reactive_power)),type="l",
+     xlab="datetime", ylab="Global_reactive_power")
+
+## here the created plot is written into a png-file.
+
+dev.copy(png, file="plot4.png", width=480, height=480)
+dev.off()
